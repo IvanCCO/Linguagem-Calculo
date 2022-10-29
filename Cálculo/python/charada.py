32df3e9e0f3922a7a0ce26621dbcd35c8deccae9
IvanCCO <ivan.miranda@sptech.school>
2022-10-29 20:02:04 -0300

    Jogo da forca completo

--- Cálculo/python/charada.py
+++ Cálculo/python/charada.py
@@ -1,9 +1,21 @@
 from random import randint
 import os
 import time
+import csv
 
 
-palavras = ['Repolho', 'Nabo', 'Rabanete', 'Cenoura']
+arr = []
+with open("..\Documentos\palavras.csv", 'r') as file:
+  csvreader = csv.reader(file)
+  for row in file:
+    arr.append(row)
+
+palavras = []
+
+for i in arr:
+    x = i.replace('"','')
+    palavras.append(x)
+
 
 def sortear():
     return randint(0,len(palavras)-1)
@@ -19,9 +31,9 @@ def limpar_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def is_letra_repetifa(letra):
+def is_letra_repetida(letra):
     for k in arr_palavra_linha:
-        if(k.lower()==letra.lowera()):
+        if(k.lower()==letra.lower()):
             print("Letra repetida")
             return True
 
@@ -32,6 +44,7 @@ def acerto(letra):
     for i in range(0,len(arr_palavra_certa)):
         if arr_palavra_certa[i].lower() == letra.lower():
             arr_palavra_linha[i] = arr_palavra_certa[i]
+           
 
 
 def is_palavra_terminada():
@@ -41,65 +54,96 @@ def is_palavra_terminada():
             return False
     return True
 
-    
+def limiter():
+    print("="*60)
+
+print("""   
+ _   _      _ _       _ _
+| | | | ___| | | ___ | | |
+| |_| |/ _ \ | |/ _ \| | |
+|  _  |  __/ | | (_) |_|_|
+|_| |_|\___|_|_|\___/(_|_)
+""")
+time.sleep(1)
+print("""Esse jogo foi criado para simplesmente passar o tempo e desenvolver minhas habiliades de lógica."
+Você primeiramente digitará a quantiade de Vezes que desejará jogar e então o jogo já começa.
+Boa sorte e não se engane as palavras são Mais difíceis do que parece""")
+
+total_acertos = 0
 
 while True:
 
     try:
-        
+        limiter()
         n = int(input("Quantas vezes deseja jogar?\n"))
 
         for i in range(0,n):
 
             count = 0
-            
-            print("Nova palavra:")
-
-            time.sleep(1)
-
+            limiter()
+            print("Escolhendo um nova Palavra")
+            time.sleep(2)
             palavraEscondida = palavras[sortear()]
 
             arr_palavra_certa = list(palavraEscondida)
 
-            arr_palavra_linha = ["_"]*len(arr_palavra_certa)
+            tamanho_frase = len(arr_palavra_certa)
+            tamanho_frase -= 1
+
+            arr_palavra_linha = ["_"]*tamanho_frase
 
             count_acerto = len(arr_palavra_linha)
 
+            arr_respota = []
+
             while count < 6:
-
                 limpar_terminal()
-
+                print(f"Vida: {6-count}\n")
                 print(*arr_palavra_linha, sep=" ")
-                letra = input("Digite uma letra\n")
+                print("\n")
+                limiter()
+                print("Letras já utilizadas: ")
+                print(*arr_respota, sep="-")
+                limiter()
+                letra = input("Digite uma letra:\n")
 
-
-                if(not is_letra_valida(palavraEscondida, letra) or is_letra_repetifa(letra)):
+                if(not is_letra_valida(palavraEscondida, letra) or is_letra_repetida(letra)):
 
                     count += 1
-                    print(f"Agora você tem {6-count} chances")
+                    
 
                 else:
                         acerto(letra)
-                        print("Acertou")
                         count_acerto -= 1
 
-                        
+
+                arr_respota.append(letra)
 
                 if count == 6:
-                    print("Você não conseguiu descobrir a palavra")
-                    time.sleep(1)
+                    limpar_terminal()
+                    print(f"Você não conseguiu descobrir a palavra\nA palavra era:")
+                    print(*arr_palavra_certa, sep="")
+                    print("Mais sorte da próxima vez")
+                    time.sleep(3)
                     break
 
                 time.sleep(1)
 
 
                 if is_palavra_terminada():
+                    limpar_terminal()
                     print("Parabéns")
+                    print(*arr_palavra_certa)
+                    time.sleep(1)
+                    total_acertos+=1
                     break
         break
             
     except:
         print("Valor errado, digite novamente")
+
+
+print(f"""\nTotal de Palavras: {n}\nTotal de acertos:{total_acertos}""")
 
 
 
--- Cálculo/python/pyhton_clima.py
+++ Cálculo/python/pyhton_clima.py
@@ -1,13 +1,5 @@
-from array import array
 import csv
-import pymysql
 
 
 
-array = ["_"] * 10
-
-print(*array, sep=" ")
-
-array[4] = "A"
-
-print(*array, sep=" ")
+print(*arr)
